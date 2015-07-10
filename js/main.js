60bdce750914f65b212031f3757d8d918f619630
Charlie Zhang <peidong97@yahoo.ca>
2015-07-10 11:26:27 -0400

    initial work on website, landing page, nav bar, etc.

---
+++ js/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){    
+  //collapse navbar toggle
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+  $(".navbar-toggle").blur(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+
+  //smooth scroll
+  $("a[href*=#]").click(function(){
+    console.log("click");
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+      return false;
+  });
+
+  //navbar scroll change
+  var scroll_start = 0;
+  var startchange = $('#startchange');
+  console.log(startchange);
+  var offset = startchange.offset();
+  if (startchange.length){
+    $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+        TweenLite.to(".navbar", 0.5, {backgroundColor:"white"});
+
+        $(".navbar-default .navbar-nav > li > a").css('color', '#525252');
+
+        $(".navbar-default .navbar-nav > li > a").mouseover(function() {
+          $(this).css("color","black")
+        }).mouseout(function(){
+          $(this).css("color", "#525252");
+        });
+
+        $(".navbar-default .navbar-nav > li > a").focus(function(){
+          $(this).css("color", "black");
+        }).blur(function(){
+          $(this).css("color", "#525252");
+        });
+
+        $(".navbar .navbar-brand").css("color", "black");
+
+        if($(document).width() < 800)
+          $(".navbar-toggle").show();
+      } 
+      else 
+      {
+        TweenLite.to(".navbar", 0.5, {backgroundColor:"transparent"});
+
+        $(".navbar-default .navbar-nav > li > a").css('color', '#E3E3E3');
+
+        $(".navbar-default .navbar-nav > li > a").mouseover(function() {
+          $(this).css("color","white")
+        }).mouseout(function(){
+          $(this).css("color", "#E3E3E3");
+        });
+
+        $(".navbar-default .navbar-nav > li > a").focus(function(){
+          $(this).css("color", "white");
+        }).blur(function(){
+          $(this).css("color", "#E3E3E3");
+        });
+
+        $(".navbar .navbar-brand").css("color", "white");
+        $(".navbar-toggle").hide();
+      }
+    });
+  }
+});
